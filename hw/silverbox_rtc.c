9bce408dab81792465366d6b156affbfea46f790
Vitaly Wool <vwool@embeddedalley.com>
2009-07-23 23:43:07 +0400

    better RTC emulation

--- hw/silverbox_rtc.c
+++ hw/silverbox_rtc.c
@@ -1,11 +1,14 @@
 #include "qemu-timer.h"
 #include "cpu.h"
+#include "irq.h"
 
 struct rtc_state {
     target_phys_addr_t base;
     uint32_t alarm_low;
     int32_t alarm_high;
     int64_t now;
+    qemu_irq irq;
+    QEMUTimer *timer;
 };
 
 /* we save the RTC for the case where the kernel is in the middle of a rtc_read
@@ -29,6 +32,13 @@ static int  silverbox_rtc_load(QEMUFile*  f, void*  opaque, int  version_id)
     /* this is an old value that is not correct. but that's ok anyway */
     s->now = qemu_get_be64(f);
     return 0;
+}
+
+static void silverbox_rtc_tm(void *opaque)
+{
+    struct rtc_state *s = opaque;
+
+    qemu_irq_raise(s->irq);
 }
 
 static uint32_t silverbox_rtc_read(void *opaque, target_phys_addr_t offset)
@@ -57,13 +67,14 @@ static void silverbox_rtc_write(void *opaque, target_phys_addr_t offset, uint32_
             s->alarm_low = value;
             alarm = s->alarm_low | (int64_t)s->alarm_high << 32;
             //printf("next alarm at %lld, tps %lld\n", alarm, ticks_per_sec);
-            //qemu_mod_timer(s->timer, alarm);
+            qemu_mod_timer(s->timer, alarm);
             break;
         case 0xc:
             s->alarm_high = value;
             //printf("alarm_high %d\n", s->alarm_high);
             break;
         case 0x10:
+            qemu_irq_lower(s->irq);
             //silverbox_device_set_irq(&s->dev, 0, 0);
             break;
         default:
@@ -85,12 +96,14 @@ static CPUWriteMemoryFunc *silverbox_rtc_writefn[] = {
     silverbox_rtc_write
 };
 
-void silverbox_rtc_init(uint32_t base)
+void silverbox_rtc_init(uint32_t base, qemu_irq irq)
 {
     int iomemtype = cpu_register_io_memory(0, silverbox_rtc_readfn,
                                            silverbox_rtc_writefn, &rtc_state);
     cpu_register_physical_memory(base, 0x100, iomemtype);
     rtc_state.base = base;
+    rtc_state.irq = irq;
+    rtc_state.timer = qemu_new_timer(vm_clock, silverbox_rtc_tm, &rtc_state);
 
     register_savevm( "silverbox_rtc", 0, SILVERBOX_RTC_SAVE_VERSION,
                      silverbox_rtc_save, silverbox_rtc_load, &rtc_state);
